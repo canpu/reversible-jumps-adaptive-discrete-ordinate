ba6a553c573e01ad6b0daf0197ab351f9b7e8d75
Can Pu <pucan@gatech.edu>
2018-06-04 11:04:50 -0500

    Modified the order of polynomials in get_fem_basis

--- src/get_fem_basis.m
+++ src/get_fem_basis.m
@@ -10,9 +10,11 @@ function polynomials = get_fem_basis(n)
     % Input: n - the degree of polynomials
     % Output: polynomials - a matrix of polynomials; each row is a separate
     %           polynomial; the first polynomial corresponds to the
-    %           polynomial associated with node (1, 1) and polynomials are
-    %           ordered in a counterclockwise order; for example, for n =
-    %           1, the four polynomials are for (1, 1), (-1, 1), (-1, -1)
-    %           and (1, -1) in order
+    %           polynomial associated with node (-1, -1) and polynomials are
+    %           ordered in a clockwise order; for example, for n =
+    %           1, the four polynomials are for (-1, -1), (-1, 1), (1, 1)
+    %           and (1, -1) in order; for n = 2, the eight polynomials are
+    %           for (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1,
+    %           -1), (0, -1) respectively
     %% Replace this section with code
 end
